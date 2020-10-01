649799c432818bb053bdaa1e06865bebbb877e9b
Nahum-png <nahuolvera@gmail.com>
2020-10-01 08:42:15 -0500

    Commit de Ejercicio

--- src/uaslp/enginering/labs/Checkbox.java
+++ src/uaslp/enginering/labs/Checkbox.java
@@ -1,4 +1,22 @@
 package uaslp.enginering.labs;
 
 public class Checkbox {
+    private String text;
+
+    public Checkbox(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void draw(){
+        System.out.print("[""]" + text);
+    }
+
 }
--- src/uaslp/enginering/labs/ListBox.java
+++ src/uaslp/enginering/labs/ListBox.java
@@ -1,4 +1,6 @@
 package uaslp.enginering.labs;
 
 public class ListBox {
+    public String option;
+
 }
